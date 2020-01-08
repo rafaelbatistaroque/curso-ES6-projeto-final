64f15d6b74d9bdcc252876696430ddadcf4052d3
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-08 00:45:21 -0400

    Horário de expediente refatorado em classe.

--- js/modules/expediente.js
+++ js/modules/expediente.js
@@ -1,16 +1,32 @@
-export default function mostrarExpediente() {
-    const expediente = document.querySelector('[data-semana]');
-    const diasExpediente = expediente.dataset.semana.split(',').map(Number);
-    const horasExpediente = expediente.dataset.horario.split(',').map(Number);
-
-    const dataAtual = new Date();
-    const diaAtual = dataAtual.getDay();
-    const horaAtual = dataAtual.getHours();
-
-    const diaAberto = diasExpediente.indexOf(diaAtual) !== -1;
-    const horaAberto = horaAtual >= horasExpediente[0] && horaAtual < horasExpediente[1];
-
-    diaAberto && horaAberto ?
-        expediente.classList.remove('fechado') :
-        expediente.classList.add('fechado');
+export default class Expediente {
+    constructor(semana) {
+        this.expediente = document.querySelector(semana);
+    }
+    carregarDadosExpediente() {
+        this.diasExpediente = this.expediente.dataset.semana.split(',').map(Number);
+        this.horasExpediente = this.expediente.dataset.horario.split(',').map(Number);
+    }
+    carregarDadosAtuais() {
+        this.dataAtual = new Date();
+        this.diaAtual = this.dataAtual.getDay();
+        this.horaAtual = this.dataAtual.getUTCHours() - 4;
+    }
+    retornarTrueSeAberto() {
+        const diaAberto = this.diasExpediente.indexOf(this.diaAtual) !== -1;
+        const horaAberto = this.horaAtual >= this.horasExpediente[0] && this.horaAtual < this.horasExpediente[1];
+        return diaAberto && horaAberto;
+    }
+    ativarSinalVerdeSeAberto() {
+        if (this.retornarTrueSeAberto())
+            this.expediente.classList.remove('fechado');
+        else this.expediente.classList.add('fechado');
+    }
+    iniciar() {
+        if (this.expediente) {
+            this.carregarDadosExpediente();
+            this.carregarDadosAtuais();
+            this.ativarSinalVerdeSeAberto();
+        }
+        return this;
+    }
 }
